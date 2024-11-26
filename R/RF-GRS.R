47fbc1b81d26af57d15b338e1323a4db481c9f46
Vaishnavi Venkat <vvenkat6@users.noreply.github.com>
2024-11-26 00:36:37 -0500

    Added code to process summary statistic file

--- R/RF-GRS.R
+++ R/RF-GRS.R
@@ -1,13 +1,23 @@
-
 #' Perform SNP Analysis
 #'
-#' This function calculated genetic risk score using random forest when there is no base data available
+#' This function calculates genetic risk score using random forest when there is no base data available
 #'
 #' @param bedfile Path to the BED file.
 #' @param seed Seed for random number generation.
+#' @param r2_grid R2 grid for clumping.
+#' @param sumstats_file Path to the summary statistics file.
 #' @return A list containing the analysis results.
 #' @export
-snp_analysis <- function(bedfile, seed = 123) {
+
+snp_analysis <- function(bedfile = NULL, seed = 123, r2_grid = c(0.01, 0.05, 0.1, 0.2, 0.5, 0.8, 0.95), sumstats_file = NULL, NCORES = 4) {
+
+  set.seed(seed)  
+
+  if (is.null(bedfile)) {
+  	print("Error: Please provide bedfile.")
+	quit(status = 1)
+  }
+
   tmpfile <- tempfile()
   snp_readBed(bedfile, backingfile = tmpfile)
   obj.SNP <- snp_attach(paste0(tmpfile, '.res'))
@@ -15,42 +25,57 @@ snp_analysis <- function(bedfile, seed = 123) {
   CHR <- obj.SNP$map$chromosome
   POS <- obj.SNP$map$physical.pos
 
-  # Split data into train and validation set varying with seed
-  train.ratio <- 0.8
-  set.seed(seed)
-  ind.train <- sample(nrow(G), round(train.ratio*nrow(G)))
-  ind.test <- setdiff(rows_along(G), ind.train)
+  if (!is.null(sumstats_file)) {
+  	# Process summary statistics file
+  	print("Processing summary statistics file...")
 
-  NCORES = 4 
+	if ("P" %in% colnames(sumstats_file)) {
+      		print("Found 'P' column in summary statistics file")
+      		# Process using 'P' column
+		lpS <- -log10(sumstats_file$P)
+    	} else if ("lpS" %in% colnames(sumstats_file)) {
+      		print("Found 'lpS' column in summary statistics file")
+    		lpS <- sumstats_file$lpS
+    	} else {
+      	log_print("Error: Summary statistics file must contain a column named 'P' for p-values or 'lpS' for -log10(P)")
+      	quit(status = 1)
+    	}
+	train.ratio <- 0.8
+        ind.train <- sample(nrow(G), round(train.ratio*nrow(G)))
+        ind.test <- setdiff(rows_along(G), ind.train)
 
-  # C+T prep
-  gwas.train <- big_univLogReg(G,tPheno[ind.train],ind.train = ind.train)
-  gwas_gc <- snp_gc(gwas.train)
-  p.value.all <- predict(gwas.train, log10 = FALSE)
-  thrs <- sort(c(0, -log10(5e-8), exp(seq(log(0.1), log(100), length.out = 100))))
-  lpS <- -predict(gwas_gc)
+  } else {
 
-  # p-value grid:
-  gwas_gc$lpS <- lpS
-  rownames(gwas_gc) <- obj.SNP$map[,2]
+  	print("No summary statistics file provided, splitting given data into training and test set to compute GRS...")
+
+  	# Split data into train and validation set varying with seed
+  	train.ratio <- 0.8
+  	ind.train <- sample(nrow(G), round(train.ratio*nrow(G)))
+  	ind.test <- setdiff(rows_along(G), ind.train)
+
+  	# C+T prep
+  	gwas.train <- big_univLogReg(G,tPheno[ind.train],ind.train = ind.train)
+  	gwas_gc <- snp_gc(gwas.train)
+  	p.value.all <- predict(gwas.train, log10 = FALSE)
+  	thrs <- sort(c(0, -log10(5e-8), exp(seq(log(0.1), log(100), length.out = 100))))
+  	lpS <- -predict(gwas_gc)
+
+  	# p-value grid:
+  	gwas_gc$lpS <- lpS
+  	rownames(gwas_gc) <- obj.SNP$map[,2]
+  }
 
   x <- G[]
   colnames(x) <- obj.SNP$map[,2]
   rownames(x) <- obj.SNP$fam[,1]
 
-  pred.Train.cp <- data.frame(matrix(ncol = 800, nrow = 0))
-  pred.Test.cp <- data.frame(matrix(ncol = 200, nrow = 0))
-
+  pred.Train.cp <- data.frame(matrix(ncol = length(ind.train), nrow = 0))
+  pred.Test.cp <- data.frame(matrix(ncol = length(ind.test), nrow = 0))
   rfnr2 <- NULL
-  rfauc1 <- NULL
-  rfauc2 <- NULL
-  rfauc3 <- NULL
-  rfn <- NULL
-  rfc <- NULL
-  rfp <- NULL
-  rfi <- NULL
 
   count = 0
+
+  # Run clumping 
   all_keep <- snp_grid_clumping(G, CHR, POS, ind.row = ind.train, lpS = lpS,grid.base.size = c(50, 100, 200),exclude = which(is.na(lpS)),ncores = NCORES)
 
   for (idx in all_keep){
@@ -67,7 +92,7 @@ snp_analysis <- function(bedfile, seed = 123) {
       if (p>3){
         hyper_grid <- expand.grid(
           mtry = c(sqrt(p)/2,sqrt(p),sqrt(p)*2,0.1 * p,0.5 * p),
-          num.trees = c(250, 500),
+          num.trees = c(5000),
           OOB_RMSE   = 0
         )
         for(i in 1:nrow(hyper_grid)){
@@ -85,12 +110,6 @@ snp_analysis <- function(bedfile, seed = 123) {
         pred.Train.cp <- rbind(pred.Train.cp,pred.Train)
         pred.Test.cp <- rbind(pred.Test.cp,pred.Test)
         rfnr2 <- rbind(rfnr2,round(NagelkerkeR2(glm(tPheno[ind.test]~pred.Test,family = 'binomial'))$R2, 4))
-        rfauc1 <- rbind(rfauc1,round(AUCBoot(pred.Test,tPheno[ind.test])[[1]],4))
-        rfauc2 <- rbind(rfauc2,round(AUCBoot(pred.Test,tPheno[ind.test])[[2]],4))
-        rfauc3 <- rbind(rfauc3,round(AUCBoot(pred.Test,tPheno[ind.test])[[3]],4))
-        rfn <- rbind(rfn,p)
-        rfc <- rbind(rfc,count)
-        rfp <- rbind(rfp,pcut)
       }
     }
   }
@@ -99,16 +118,6 @@ snp_analysis <- function(bedfile, seed = 123) {
 
   id = which.max(rfnr2)
   rf.nr2 <- rfnr2[id]
-  rf.auc.a <- rfauc1[id]
-  rf.auc.b <- rfauc2[id]
-  rf.auc.c <- rfauc3[id]
-  rf.n <- rfn[id]
 
-  return(list(
-    rf.nr2 = rf.nr2,
-    rf.auc.a = rf.auc.a,
-    rf.auc.b = rf.auc.b,
-    rf.auc.c = rf.auc.c,
-    rf.n = rf.n
-  ))
+  return(pred.Test[id,])
 }
